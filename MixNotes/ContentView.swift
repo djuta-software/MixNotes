bc168f71226d67d351b0c2bb5b632d73e6e7031e
Deven Juta <deven.juta@cbc.ca>
2020-04-28 22:07:22 -0400

    Initial Commit

---
+++ MixNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MixNotes
+//
+//  Created by Deven Juta on 2020-04-28.
+//  Copyright © 2020 Deven Juta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
